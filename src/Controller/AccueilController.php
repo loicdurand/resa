9154f6e643ec763d14c97659156a0228f4571949
Loïc DURAND <loicdurand.ld@gmail.com>
2024-11-08 23:36:57 -0400

    Affichage d e la liste des véhicules propre

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,10 +1,25 @@
 <?php
-// src/Controller/LuckyController.php
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Role;
+use App\Entity\Action;
+use App\Entity\CarburantVehicule;
+use App\Entity\CategorieVehicule;
+use App\Entity\GenreVehicule;
+use App\Entity\Permission;
+use App\Entity\TransmissionVehicule;
+use App\Entity\Vehicule;
+use App\Form\VehiculeType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
 
 
 class AccueilController extends AbstractController
@@ -12,14 +27,17 @@ class AccueilController extends AbstractController
     private $app_const;
 
     #[Route('/')]
-    public function accueil(): Response
+    public function accueil(ManagerRegistry $doctrine): Response
     {
         $this->setAppConst();
 
-        $number = random_int(0, 100);
+        $em = $doctrine->getManager();
+        $vehicules = $em
+            ->getRepository(Vehicule::class)
+            ->findAll();
 
         return $this->render('accueil/liste.html.twig', array_merge($this->getAppConst(), [
-            'number' => $number,
+            'vehicules' => $vehicules,
         ]));
     }
 
