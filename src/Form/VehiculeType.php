dd61998c943563f5d371b07414f2d940ea10cead
Loïc DURAND <loicdurand.ld@gmail.com>
2024-11-10 00:57:36 -0400

    pls fixs affichage form sur mobile

--- src/Form/VehiculeType.php
+++ src/Form/VehiculeType.php
@@ -19,39 +19,39 @@ class VehiculeType extends AbstractType
 
         $builder
             ->add('marque', null, [
-                'row_attr' => ['class' => 'fr-col-sm-12'],
+                'row_attr' => ['class' => 'fr-col-12 fr-col-sm-12'],
                 'help_attr' => ['content' => 'Obligatoire (255 caractères maximum).']
             ])
             ->add('modele', null, [
-                'row_attr' => ['class' => 'fr-col-sm-12'],
+                'row_attr' => ['class' => 'fr-col-12 fr-col-sm-12'],
                 'help_attr' => ['content' => 'Obligatoire (255 caractères maximum).']
             ])
             ->add('motorisation', null, [
-                'row_attr' => ['class' => 'fr-col-sm-6'],
+                'row_attr' => ['class' => 'fr-col-6 fr-col-sm-6'],
                 'help_attr' => ['content' => 'Facultatif (25 caractères maximum).'],
                 'attr' => [
                     'placeholder' => '200CDI, 30D, 1.5DCi 75, etc...'
                 ]
             ])
             ->add('finition', null, [
-                'row_attr' => ['class' => 'fr-col-sm-6'],
+                'row_attr' => ['class' => 'fr-col-6 fr-col-sm-6'],
                 'help_attr' => ['content' => 'Faculatif (25 caractères maximum).'],
                 'attr' => [
                     'placeholder' => 'Privilège, Elegance, etc...'
                 ],
             ])
             ->add('immatriculation', null, [
-                'row_attr' => ['class' => 'fr-col-sm-4'],
+                'row_attr' => ['class' => 'fr-col-12 fr-col-sm-12'],
                 'attr' => ['class' => 'fr-input'],
                 'help_attr' => ['content' => 'Obligatoire (9 caractères maximum).']
             ])
             ->add('controle_technique', null, [
-                'row_attr' => ['class' => 'fr-col-sm-4'],
+                'row_attr' => ['class' => 'fr-col-6 fr-col-sm-6'],
                 'widget' => 'single_text',
-                'help_attr' => ['content' => 'Facultatif (format: JJ/MM/AAAA).'],
+                'help_attr' => ['content' => 'Facultatif.'],
             ])
             ->add('NbPlaces', null, [
-                'row_attr' => ['class' => 'fr-col-sm-4'],
+                'row_attr' => ['class' => 'fr-col-6 fr-col-sm-6'],
                 'help_attr' => ['content' => 'Faculatif.'],
                 'label' =>  'Nombre de places',
             ])
@@ -62,28 +62,28 @@ class VehiculeType extends AbstractType
             ->add('genre', EntityType::class, [
                 'class' => GenreVehicule::class,
                 'choice_label' => 'code',
-                'row_attr' => ['class' => 'fr-select-group fr-col-sm-3'],
+                'row_attr' => ['class' => 'fr-select-group fr-col-6 fr-col-sm-6'],
                 'label_attr' => ['class' => 'fr-label'],
                 'help_attr' => ['content' => 'Faculatif.'],
             ])
             ->add('categorie', EntityType::class, [
                 'class' => CategorieVehicule::class,
                 'choice_label' => 'libelle',
-                'row_attr' => ['class' => 'fr-select-group fr-col-sm-3'],
+                'row_attr' => ['class' => 'fr-select-group fr-col-6 fr-col-sm-6'],
                 'label_attr' => ['class' => 'fr-label'],
                 'help_attr' => ['content' => 'Faculatif.'],
             ])
             ->add('transmission', EntityType::class, [
                 'class' => TransmissionVehicule::class,
                 'choice_label' => 'libelle',
-                'row_attr' => ['class' => 'fr-select-group fr-col-sm-3'],
+                'row_attr' => ['class' => 'fr-select-group fr-col-6 fr-col-sm-6'],
                 'label_attr' => ['class' => 'fr-label'],
                 'help_attr' => ['content' => 'Faculatif.'],
             ])
             ->add('carburant', EntityType::class, [
                 'class' => CarburantVehicule::class,
                 'choice_label' => 'libelle',
-                'row_attr' => ['class' => 'fr-select-group fr-col-sm-3'],
+                'row_attr' => ['class' => 'fr-select-group fr-col-6 fr-col-sm-6'],
                 'label_attr' => ['class' => 'fr-label'],
                 'help_attr' => ['content' => 'Faculatif.']
             ])
