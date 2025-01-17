040e1359353a85ab07a5ac6873316206d0084288
Loic DURAND <loicdurand.ld@gmail.com>
2025-01-16 21:24:32 -0400

    activation du mode debug et ajout de MakerBundle pour l'environnement chrome

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['chrome' => true, 'dev' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
