fc2dfe23255ab99fe3f0abd90e743e6f343c06b1
Loïc DURAND <loicdurand.ld@gmail.com>
2025-11-10 20:04:16 +0000

    Régénération migration suite changement de machine

--- migrations/Version20251110195931.php
+++ migrations/Version20251110195931.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251017154640 extends AbstractMigration
+final class Version20251110195931 extends AbstractMigration
 {
     public function getDescription(): string
     {
