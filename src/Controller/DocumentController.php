4a6c55108e89b4e95fcbd0060b6cef1d98b826b1
Loïc DURAND <loicdurand.ld@gmail.com>
2026-01-12 19:57:58 +0000

    lien de telechargement fiche de reintegration

--- src/Controller/DocumentController.php
+++ src/Controller/DocumentController.php
@@ -8,8 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class DocumentController extends AbstractController
 {
-    #[Route('/fiche_perception', name: 'resa_perception')]
-    public function getDocumentAction(): Response
+    #[Route('/fiche_perception', name: 'resa_document_perception')]
+    public function perception(): Response
     {
         // Lorsque l'utilisateur clique sur le lien de téléchargement, on lui renvoie le document se trouvant dans /assets/pdf/perception.pdf
         $filePath = $this->getParameter('kernel.project_dir') . '/assets/pdf/perception.pdf';
@@ -17,6 +17,17 @@ final class DocumentController extends AbstractController
         $response->setContent(file_get_contents($filePath));
         $response->headers->set('Content-Type', 'application/pdf');
         $response->headers->set('Content-Disposition', 'attachment;filename="perception.pdf"');
+        return $response;
+    }
+
+    #[Route('/fiche_reintegration', name: 'resa_document_reintegration')]
+    public function reintegration(): Response
+    {
+        $filePath = $this->getParameter('kernel.project_dir') . '/assets/pdf/perception.pdf';
+        $response = new Response();
+        $response->setContent(file_get_contents($filePath));
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Content-Disposition', 'attachment;filename="reintegration.pdf"');
         return $response;
     }
 }
